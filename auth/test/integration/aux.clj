46e652f1600bcb5bec92869efd7e890d645a627d
alefeans <alefeans2@hotmail.com>
2023-02-09 13:42:09 -0300

    improving test aux ns

--- auth/test/integration/aux.clj
+++ auth/test/integration/aux.clj
@@ -1,23 +1,24 @@
 (ns integration.aux
-  (:require [io.pedestal.http :as http]
+  (:require [auth.diplomat.http-server :as d-http]
+            [cheshire.core :as json]
+            [clojure.test :refer :all]
+            [com.stuartsierra.component :as component]
+            [io.pedestal.http :as http]
             [io.pedestal.http.route :as route]
             [io.pedestal.test :refer [response-for]]
-            [com.stuartsierra.component :as component]
-            [clojure.test :refer :all]
-            [auth.diplomat.http-server :as d-http]
-            [cheshire.core :as json]))
+            [schema.core :as s]))
+
+(defmacro with-test-system [[sut system] & body]
+  `(let [~sut (component/start (~system :test))]
+     (try
+       (s/with-fn-validation ~@body)
+       (finally
+         (component/stop ~sut)))))
 
 (def url-for (route/url-for-routes d-http/routes))
 
 (defn service-fn [system]
   (get-in system [:server :server ::http/service-fn]))
-
-(defmacro with-test-system [[sut system] & body]
-  `(let [~sut (component/start (~system :test))]
-     (try
-       ~@body
-       (finally
-         (component/stop ~sut)))))
 
 (defn json-response-for [system verb url & options]
   (-> (apply response-for (service-fn system) verb url options)
