b666091cde97ad04975baf779c711016bdaa534c
alefeans <alefeans2@hotmail.com>
2023-02-10 10:21:45 -0300

    adding support to adapt array fields

--- auth/src/auth/adapters/user.clj
+++ auth/src/auth/adapters/user.clj
@@ -3,7 +3,14 @@
             [auth.wire.in.user :as in.user]
             [auth.wire.db.user :as db.user]
             [auth.wire.out.user :as out.user]
-            [auth.models.user :as m.user]))
+            [auth.models.user :as m.user]
+            [next.jdbc.result-set :as rs])
+  (:import [java.sql Array]))
+
+(extend-protocol rs/ReadableColumn
+  Array
+  (read-column-by-label [^Array v _]    (vec (.getArray v)))
+  (read-column-by-index [^Array v _ _]  (vec (.getArray v))))
 
 (s/defn wire-create-user->internal :- m.user/User
   [user :- in.user/CreateUser]
@@ -15,4 +22,4 @@
 
 (s/defn internal->db :- db.user/User
   [user :- m.user/User]
-  user)
+  (update user :roles #(into-array String %)))
