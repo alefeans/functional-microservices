2d739a9bfd4937428650e30681505788763124aa
alefeans <alefeans2@hotmail.com>
2023-02-10 10:22:34 -0300

    adjusting diplomat db fns

--- auth/src/auth/diplomat/db.clj
+++ auth/src/auth/diplomat/db.clj
@@ -9,16 +9,16 @@
 (s/defn user-exists? :- s/Bool
   [{conn :connection}
    {email :email} :- m.user/User]
-  (:exists (jdbc/execute-one! conn ["select exists(select 1 from test where email = ?)" email])))
+  (:exists (jdbc/execute-one! conn ["select exists(select 1 from users where email = ?)" email])))
 
 (s/defn create-user! :- m.user/User
   [{conn :connection}
    user :- m.user/User]
-  (sql/insert! conn :test (a.user/internal->db user))
+  (sql/insert! conn :users (a.user/internal->db user))
   user)
 
-;; TODO adapt input/output from these DIPLOMATs and LOG the jdbc execution outputs
-;; (s/defn get-user! :- (s/maybe m.user/User)
-;;   [{conn :connection}
-;;    user :- m.user/User]
-;;   (jdbc/execute-one! conn ["select * from test where email = ?" (:email user)] {:builder-fn rs/as-unqualified-maps}))
+;; TODO fix this fn
+(s/defn get-user! :- (s/maybe m.user/User)
+  [{conn :connection}
+   user :- m.user/User]
+  (jdbc/execute-one! conn ["select * from users where email = ?" (:email user)] {:builder-fn rs/as-unqualified-maps}))
